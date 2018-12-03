e6aab09c203408927eabd27759d34e2bcd22e7e0
eddast <eddasr15@github.com>
2018-12-03 14:22:26 +0000

    Refactors template code to my needs

--- game-api/deck.js
+++ game-api/deck.js
@@ -2,7 +2,7 @@
  * The deck for lucky21 game
  * represents cards in a deck in the form of a string array
  */
-module.exports = () => {
+ const deck = () => {
     return [
       '01H', '02H', '03H', '04H', '05H', '06H', '07H', '08H', '09H', '10H', '11H', '12H', '13H', // Hearts
       '01C', '02C', '03C', '04C', '05C', '06C', '07C', '08C', '09C', '10C', '11C', '12C', '13C', // Clubs
@@ -10,3 +10,5 @@ module.exports = () => {
       '01S', '02S', '03S', '04S', '05S', '06S', '07S', '08S', '09S', '10S', '11S', '12S', '13S', // Spades
     ];
 };
+
+module.exports = deck;
--- game-api/lucky21.unit-test.js
+++ game-api/lucky21.unit-test.js
@@ -9,7 +9,6 @@ const lucky21Constructor = require('./lucky21.js');
 /***********************
  * TEST INITIALIZATION *
  ***********************/
-
 test('a new game should have 50 cards left in the deck', () => {
   let deck = deckConstructor();
   let dealer = dealerConstructor();
@@ -24,25 +23,23 @@ test('a new game should have 2 drawn cards', () => {
   expect(game.state.cards.length).toEqual(2);
 });
 
-
 /**********************
  * TEST FUNCTIONALITY *
  **********************/
-
-test('guess21OrUnder should draw the next card', () => {
-  // Arrange
-  let deck = deckConstructor();
-  let dealer = dealerConstructor();
-  deck = [ '10H', '09S', '01D', '05L', ];
-  dealer.shuffle = deck => { /* Override the shuffle to do nothing */ };
-  let game = lucky21Constructor(deck, dealer);
+// test('guess21OrUnder should draw the next card', () => {
+//   // Arrange
+//   let deck = deckConstructor();
+//   let dealer = dealerConstructor();
+//   deck = [ '10H', '09S', '01D', '05L', ];
+//   dealer.shuffle = deck => { /* Override the shuffle to do nothing */ };
+//   let game = lucky21Constructor(deck, dealer);
   
-  // Act
-  game.guess21OrUnder(game, dealer);
+//   // Act
+//   game.guess21OrUnder(game, dealer);
   
-  // Assert
-  expect(game.state.cards.length).toEqual(3);
-  expect(game.state.cards[2]).toEqual('01D');
-});
+//   // Assert
+//   expect(game.state.cards.length).toEqual(3);
+//   expect(game.state.cards[2]).toEqual('01D');
+// });
 
 // TODO: At least 20 unit tests
