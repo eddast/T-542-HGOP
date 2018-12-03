14912d8bfae6a5c3604e159b8de27a835aaed761
eddast <eddasr15@github.com>
2018-12-03 13:29:14 +0000

    Modify functionality setup and unit tests to use dependency injection

--- game-api/lucky21.unit-test.js
+++ game-api/lucky21.unit-test.js
@@ -1,11 +1,25 @@
-const lucky21Constructer = require('./lucky21.js');
+const deckConstructor = require('./deck.js');
+const dealerConstructor = require('./dealer.js');
+const lucky21Constructor = require('./lucky21.js');
 
-test('a new game should have 50 cards left in the deck', () => {
-  let game = lucky21Constructer();
-  expect(game.deck.length).toEqual(50);
+test('guess21OrUnder should draw the next card', () => {
+  // Arrange
+  let deck = deckConstructor();
+  let dealer = dealerConstructor();
+  deck = [ '10H', '09S', '01D', '05L', ];
+
+  // Override the shuffle to do nothing.
+  dealer.shuffle = (deck) => {};
+  
+  // Inject our dependencies
+  let game = lucky21Constructor(deck, dealer);
+  
+  // Act
+  game.guess21OrUnder(game, dealer);
+  
+  // Assert
+  expect(game.cards.length).toEqual(3);
+  expect(game.cards[2]).toEqual('01D');
 });
 
-test('a new game should have 2 drawn cards', () => {
-  let game = lucky21Constructer();
-  expect(game.cards.length).toEqual(2);
-});
+// TODO: At least 20 unit tests
