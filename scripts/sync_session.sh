79ec1f9e39b3c31ca9d33ee4b7250432ab45568f
eddast <eddasr15@github.com>
2018-12-04 15:18:28 +0000

    Adds a executable sync_session.sh script to copy credentials to jenkins instance

---
+++ scripts/sync_session.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+JENKINS_URL="ec2-54-159-65-134.compute-1.amazonaws.com"
+
+scp -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ~/.aws/credentials ubuntu@${JENKINS_URL}:~/credentials
+ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo mv ~/credentials /var/lib/jenkins/.aws/credentials"
+ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo chmod a+r /var/lib/jenkins/.aws/credentials"
