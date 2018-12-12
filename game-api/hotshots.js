3a82d6ff652eac9251d7743063fa103c388e48b4
eddast <eddasr15@github.com>
2018-12-12 18:43:03 +0000

    adds hotshot dependencies

---
+++ game-api/hotshots.js
@@ -0,0 +1,8 @@
+/**
+ * Creates hotshot client initialized with our data dog container
+ * @param context the context/dependencies module uses
+ */
+module.exports = (context) => {
+    const HotShots = context("hotshots");
+    return HotShots('my_datadog_container').bind(this);
+};
