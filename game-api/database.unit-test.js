4117bc202039da459125579ef01677e7a98d668e
eddast <eddasr15@github.com>
2018-12-14 19:52:34 +0000

    Fixes database unit test lint errors and adds comments

--- game-api/database.unit-test.js
+++ game-api/database.unit-test.js
@@ -1,34 +1,37 @@
 const inject = require('./inject.js');
 const databaseConstructor = require('./database.js');
-
+/**
+ * Dummy object for parameter list filling
+ */
 const baseConfig = {
-    pgClient: function(config) {
-        return {
-            connect: (cb) => cb(false),
-            query: (str, cb) => { cb(false, {})},
-            end: () => {},
-        }
-    },
     config: (context) => {
         return {
             pgHost: 'localhost',
             pgUser: 'test',
             pgPassword: 'testing',
             pgDatabase: 'game'
-        }
+        };
     }
-}
-
+};
+/**
+ * Tests suite for getCountOfGameResult function suite
+ */
 describe('getCountOfGameResult functionality', () => {
+    /**
+     * Test if error callback functions as expected
+     * Whenn client cannot connect
+     */
     test('Callbacks error when the client is unable to connect with getTotalNumberOfGames', () => {
-        const newConfig = { ...baseConfig };
-        newConfig.pgClient = function() {
-            return {
-                connect: (cb) => cb(true),
-                query: (str, cb) => { cb(false, {})},
-                end: () => {},
-            }
-        }
+        const newConfig = {
+            pgClient: function() {
+                return {
+                    connect: (cb) => cb(true),
+                    query: (str, cb) => { cb(false, {}); },
+                    end: () => {},
+                };
+            },
+            config: baseConfig.config
+        };
         const successMock = jest.fn();
         const failureMock = jest.fn();
         const database = databaseConstructor(inject(newConfig));
@@ -36,16 +39,21 @@ describe('getCountOfGameResult functionality', () => {
         database.getTotalNumberOfGames(successMock, failureMock);
         expect(failureMock).toHaveBeenCalledWith(true);
     });
-
+    /**
+     * Test if method of getting total played games returns correct result
+     * given one played game and a successful connection to client
+     */
     test('Callbacks with count 1 when the client is able to query with getTotalNumberOfGames', () => {
-        const newConfig = { ...baseConfig };
-        newConfig.pgClient = function() {
-            return {
-                connect: (cb) => cb(false),
-                query: (str, cb) => { cb(false, { rows: [{ count: 1 }] }) },
-                end: () => {},
-            }
-        }
+        const newConfig = {
+            pgClient: function() {
+                return {
+                    connect: (cb) => cb(false),
+                    query: (str, cb) => { cb(false, { rows: [{ count: 1 }] }); },
+                    end: () => {},
+                };
+            },
+            config: baseConfig.config,
+        };
         const successMock = jest.fn();
         const failureMock = jest.fn();
         const database = databaseConstructor(inject(newConfig));
@@ -53,16 +61,21 @@ describe('getCountOfGameResult functionality', () => {
         database.getTotalNumberOfGames(successMock, failureMock);
         expect(successMock).toHaveBeenCalledWith(1);
     });
-
+    /**
+     * Test if method of getting total won games returns correct result
+     * given two won games and a successful connection to client
+     */
     test('Callbacks with count 2 when the client is able to query with getTotalNumberOfWins', () => {
-        const newConfig = { ...baseConfig };
-        newConfig.pgClient = function() {
-            return {
-                connect: (cb) => cb(false),
-                query: (str, cb) => { cb(false, { rows: [{ count: 2 }] }) },
-                end: () => {},
-            }
-        }
+        const newConfig = {
+            pgClient: function() {
+                return {
+                    connect: (cb) => cb(false),
+                    query: (str, cb) => { cb(false, { rows: [{ count: 2 }] }); },
+                    end: () => {},
+                };
+            },
+            config: baseConfig.config
+        };
         const successMock = jest.fn();
         const failureMock = jest.fn();
         const database = databaseConstructor(inject(newConfig));
@@ -70,16 +83,21 @@ describe('getCountOfGameResult functionality', () => {
         database.getTotalNumberOfWins(successMock, failureMock);
         expect(successMock).toHaveBeenCalledWith(2);
     });
-
+    /**
+     * Test if method of getting total games with total of 21 returns correct result
+     * given one such game and a successful connection to client
+     */
     test('Callbacks with count 1 when the client is able to query with getTotalNumberOf21', () => {
-        const newConfig = { ...baseConfig };
-        newConfig.pgClient = function() {
-            return {
-                connect: (cb) => cb(false),
-                query: (str, cb) => { cb(false, { rows: [{ count: 1 }] }) },
-                end: () => {},
-            }
-        }
+        const newConfig = { 
+            pgClient: function() {
+                return {
+                    connect: (cb) => cb(false),
+                    query: (str, cb) => { cb(false, { rows: [{ count: 1 }] }); },
+                    end: () => {},
+                };
+            },
+            config: baseConfig.config
+        };
         const successMock = jest.fn();
         const failureMock = jest.fn();
         const database = databaseConstructor(inject(newConfig));
@@ -87,16 +105,21 @@ describe('getCountOfGameResult functionality', () => {
         database.getTotalNumberOf21(successMock, failureMock);
         expect(successMock).toHaveBeenCalledWith(1);
     });
-
+    /**
+     * Test if method of getting total games with total of 21 correctly calls error callback
+     * given unsuccessful connection to client
+     */
     test('Callbacks with error when the query responds with an error with getTotalNumberOf21', () => {
-        const newConfig = { ...baseConfig };
-        newConfig.pgClient = function() {
-            return {
-                connect: (cb) => cb(false),
-                query: (str, cb) => { cb(true, { }) },
-                end: () => {},
-            }
-        }
+        const newConfig = { 
+            pgClient: function() {
+                return {
+                    connect: (cb) => cb(false),
+                    query: (str, cb) => { cb(true, { }); },
+                    end: () => {},
+                };
+            },
+            config: baseConfig.config,
+        };
         const successMock = jest.fn();
         const failureMock = jest.fn();
         const database = databaseConstructor(inject(newConfig));
@@ -105,18 +128,25 @@ describe('getCountOfGameResult functionality', () => {
         expect(failureMock).toHaveBeenCalled();
     });
 });
-
+/**
+ * Tests suite for insertResult function
+ */
 describe('insertResult processes correctly', () => {
-    
+    /**
+     * Test if method correctly calls error callback
+     * given unsuccessful connection to client
+     */
     test('Callbacks with error when the connect responds with an error with insertResult', () => {
-        const newConfig = { ...baseConfig };
-        newConfig.pgClient = function() {
-            return {
-                connect: (cb) => cb(true),
-                query: (str, cb) => { cb(false, { }) },
-                end: () => {},
-            }
-        }
+        const newConfig = { 
+            pgClient: function() {
+                return {
+                    connect: (cb) => cb(true),
+                    query: (str, cb) => { cb(false, { }); },
+                    end: () => {},
+                };
+            },
+            config: baseConfig.config
+        };
         const successMock = jest.fn();
         const failureMock = jest.fn();
         const database = databaseConstructor(inject(newConfig));
@@ -124,16 +154,21 @@ describe('insertResult processes correctly', () => {
         database.insertResult(true, 21, 21, successMock, failureMock);
         expect(failureMock).toHaveBeenCalled();
     });
-
+    /**
+     * Test if method correctly calls error callback
+     * given unsuccessful query to the database
+     */
     test('Callbacks with error when the query responds with an error with insertResult', () => {
-        const newConfig = { ...baseConfig };
-        newConfig.pgClient = function() {
-            return {
-                connect: (cb) => cb(false),
-                query: (str, cb) => { cb(true, { }) },
-                end: () => {},
-            }
-        }
+        const newConfig = { 
+            pgClient: function() {
+                return {
+                    connect: (cb) => cb(false),
+                    query: (str, cb) => { cb(true, { }); },
+                    end: () => {},
+                };
+            },
+            config: baseConfig.config
+        };
         const successMock = jest.fn();
         const failureMock = jest.fn();
         const database = databaseConstructor(inject(newConfig));
@@ -141,16 +176,21 @@ describe('insertResult processes correctly', () => {
         database.insertResult(true, 21, 21, successMock, failureMock);
         expect(failureMock).toHaveBeenCalled();
     });
-
+    /**
+     * Test if method correctly calls onSuccess function
+     * If all executes correctly
+     */
     test('Callbacks with success when the query responds with no error with insertResult', () => {
-        const newConfig = { ...baseConfig };
-        newConfig.pgClient = function() {
-            return {
-                connect: (cb) => cb(false),
-                query: (str, cb) => { cb(false, { }) },
-                end: () => {},
-            }
-        }
+        const newConfig = {
+            pgClient: function() {
+                return {
+                    connect: (cb) => cb(false),
+                    query: (str, cb) => { cb(false, { }); },
+                    end: () => {},
+                };
+            },
+            config: baseConfig.config
+        };
         const successMock = jest.fn();
         const failureMock = jest.fn();
         const database = databaseConstructor(inject(newConfig));
